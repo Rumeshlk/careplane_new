634e98d7923c009810d6fab359b02f7dff3acc46
dulitha17 <dulitha@surreytechconsulting.co.uk>
2016-10-11 09:36:03 +0530

    changes by rumesh on 11/10/2016

--- src/AdminBundle/Controller/LaboratoryController.php
+++ src/AdminBundle/Controller/LaboratoryController.php
@@ -22,9 +22,10 @@ class LaboratoryController extends Controller
     /**
      * @Route("/laboratory" , name= "laboratory")
      */
-    public function showAction(){
+    public function showAction()
+    {
         $laboratory_list = $this->getDoctrine()->getRepository('AdminBundle:Laboratory')->findAll();
-        return $this->render('AdminBundle:Laboratory:laboratory.html.twig',array('laboratory' => $laboratory_list ));
+        return $this->render('AdminBundle:Laboratory:laboratory.html.twig', array('laboratory' => $laboratory_list));
     }
 
     /**
@@ -35,18 +36,56 @@ class LaboratoryController extends Controller
         $laboratory = new Laboratory;
 
         $form = $this->createFormBuilder($laboratory)
-            ->add('Laboratory_Name',TextType::class)
-            ->add('save',SubmitType::class)
+            ->add('Laboratory_Name', TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label' => 'Add', 'attr' => array('class' => 'btn btn-primary input-sm col-md-4', 'style' => 'margin-bottom:15px')))
             ->getForm();
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             DIE('Submited');
         }
 
-        return $this->render('AdminBundle:Laboratory:create.html.twig',array('form' => $form->createView()));
+        return $this->render('AdminBundle:Laboratory:create.html.twig', array('form' => $form->createView()));
     }
 
+    /**
+     * @Route("/laboratory/edit/{id}",name = "lab_edit")
+     *
+     */
+    public function editAction($id, Request $request)
+    {
+
+        $lab = $this->getDoctrine()->getRepository('AdminBundle:Laboratory')->find($id);
+
+        $lab->setLaboratoryName($lab->getLaboratoryName());
+
+        $form = $this->createFormBuilder($lab)
+            ->add('Laboratory_Name', TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('Update' => 'Add', 'attr' => array('class' => 'btn btn-primary input-sm col-md-4', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //get submitted data
+
+            $l_name = $form['Laboratory_Name']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $lab = $em->getRepository('AdminBundle:Laboratory')->find($id);
+
+            $lab->setLaboratoryName($l_name);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice', 'Laboratory Updated'
+            );
+            return $this->redirectToRoute('laboratory');
+        }
+        return $this->render('AdminBundle:Laboratory:edit.html.twig', array('form' => $form->createView()));
+
+    }
 }
 
--- src/AdminBundle/Controller/PackageController.php
+++ src/AdminBundle/Controller/PackageController.php
@@ -23,8 +23,8 @@ class PackageController extends Controller
      * @Route("/package" , name= "package")
      */
     public function showAction(){
-        $units_list = $this->getDoctrine()->getRepository('AdminBundle:Package')->findAll();
-        return $this->render('AdminBundle:Package:package.html.twig',array('package' => $units_list ));
+        $package_list = $this->getDoctrine()->getRepository('AdminBundle:Package')->findAll();
+        return $this->render('AdminBundle:Package:package.html.twig',array('package' => $package_list ));
     }
 
     /**
@@ -35,18 +35,89 @@ class PackageController extends Controller
         $Package = new Package;
 
         $form = $this->createFormBuilder($Package)
-            ->add('singular_name',TextType::class)
-            ->add('plural_name',TextType::class)
-            ->add('save',SubmitType::class)
+            ->add('singular_name',TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('plural_name',TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('save',SubmitType::class, array('label' => 'Save', 'attr' => array('class' => 'btn btn-primary input-sm col-md-4', 'style' => 'margin-bottom:15px')))
             ->getForm();
 
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            DIE('Submited');
+           // get submitted data
+            $s_name = $form['singular_name']->getData();
+            $p_name = $form['plural_name']->getData();
+
+            $Package->setSingularName($s_name);
+            $Package->setPluralName($p_name);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($Package);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice', 'Package Added'
+            );
+            return $this->redirectToRoute('package');
         }
 
         return $this->render('AdminBundle:Package:create.html.twig',array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/package/edit/{id}" ,name = "package_edit")
+     */
+    public function editAction($id, Request $request)
+    {
+        $Package = $this->getDoctrine()->getRepository('AdminBundle:Package')->find($id);
+
+        $Package->setSingularName($Package->getSingularName());
+        $Package->setPluralName($Package->getPluralName());
+
+        $form = $this->createFormBuilder($Package)
+            ->add('singular_name',TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('plural_name',TextType::class, array('attr' => array('class' => 'form-control input-sm', 'style' => 'margin-bottom:15px')))
+            ->add('save',SubmitType::class, array('label' => 'Save', 'attr' => array('class' => 'btn btn-primary input-sm col-md-4', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //get submitted data
+            $s_name = $form['singular_name']->getData();
+            $p_name = $form['plural_name']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $Package = $em->getRepository('AdminBundle:Package')->find($id);
+
+            $Package->setSingularName($s_name);
+            $Package->setPluralName($p_name);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice', 'Unit Updated'
+            );
+            return $this->redirectToRoute('package');
+        }
+        return $this->render('AdminBundle:Package:edit.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/package/delete/{id}" ,name = "package_delete")
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $unit = $em->getRepository('AdminBundle:Units')->find($id);
+
+        $em->remove($unit);
+        $em->flush();
+
+        $this->addFlash(
+            'notice', 'Unit Deleted'
+        );
+        return $this->redirectToRoute('units');
     }
 
 }
